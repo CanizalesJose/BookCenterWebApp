90fc0d31ee4bc87cf6785c91fcf28f213a47a39b
CanizalesJose <jose1605m@gmail.com>
2024-09-17 12:56:37 -0700

    Updated

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,7 +1,9 @@
 exports.renderHomePage = (req, res) => {
   console.log(`${req.method} on route ${req.path}`);
   res.render('home', {
-    activePage: "home"});
+    activePage: "home",
+    user: ""
+  });
 };
 
 exports.renderLogin = (req, res) => {
